3beaf6fe394bf5f62cf330ac69a6a4dff72961e8
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-02-14 03:50:00 +0600

    commit -5

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const showImages = (images) => {
   images.forEach((image) => {
     let div = document.createElement("div");
     div.className = "col-lg-3 col-md-4 col-xs-6 img-item mb-2";
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    div.innerHTML = `
+     <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div);
   });
 };
